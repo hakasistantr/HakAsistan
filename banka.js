ee32822373cfba54d4df9e357ee658506daf1142
hakasistantr <hakasistantr@gmail.com>
2026-08-07 19:58:20 +0300

    Create banka.js

---
+++ banka.js
@@ -0,0 +1,16 @@
+function bankaAnalizi(metin){
+
+return `
+<h2>🏦 Banka Hukuku Analizi</h2>
+
+<p>Anlattığınız konu banka ve finans hukuku kapsamında değerlendirilebilir.</p>
+
+<ul>
+<li>✅ Hesap hareketlerini saklayın.</li>
+<li>✅ Bankaya yazılı başvuru yapın.</li>
+<li>✅ Gerekirse BDDK veya Tüketici Hakem Heyetine başvurun.</li>
+</ul>
+
+`;
+
+}
