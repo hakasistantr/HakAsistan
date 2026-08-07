d70f4293ade4b650b2b29ab8ee12fb9b6a97faf4
hakasistantr <hakasistantr@gmail.com>
2026-08-07 20:03:56 +0300

    Update kira.js

--- kira.js
+++ kira.js
@@ -1,19 +1,15 @@
-function kiraAnalizi(metin){
+function kiraAnalizi(metin) {
 
-return `
-<h2>🏠 Kira Hukuku Analizi</h2>
+    return` 
+    <h2>🏠 Kira Hukuku Analizi</h2>
 
-<p>
-Anlattığınız konu kira hukuku kapsamında değerlendirilebilir.
-</p>
+    <p>Anlattığınız konu kira hukuku kapsamında değerlendirilebilir.</p>
 
-<ul>
-<li>✅ Kira sözleşmesini saklayın.</li>
-<li>✅ Depozito dekontunu saklayın.</li>
-<li>✅ Yazışmaları ve mesajları saklayın.</li>
-<li>✅ Gerekirse Sulh Hukuk Mahkemesi veya Arabuluculuk yoluna başvurabilirsiniz.</li>
-</ul>
-
-`;
-
+    <ul>
+        <li>✅ Kira sözleşmesini saklayın.</li>
+        <li>✅ Depozito dekontunu saklayın.</li>
+        <li>✅ Yazışmaları ve mesajları saklayın.</li>
+        <li>✅ Gerekirse Sulh Hukuk Mahkemesi veya Arabuluculuğa başvurabilirsiniz.</li>
+    </ul>
+    `;
 }
