9b3fc8d5fdb1d5aedae2f5c2affab5fc3c532689
hakasistantr <hakasistantr@gmail.com>
2026-08-07 20:23:01 +0300

    Update kanunlar.js

--- kanunlar.js
+++ kanunlar.js
@@ -1,1 +1,13 @@
+function kanunGetir(kelime) {
 
+const kanunlar = {
+"kira":"Türk Borçlar Kanunu",
+"iş":"4857 Sayılı İş Kanunu",
+"trafik":"2918 Sayılı Karayolları Trafik Kanunu",
+"tüketici":"6502 Sayılı Tüketicinin Korunması Hakkında Kanun",
+"aile":"4721 Sayılı Türk Medeni Kanunu"
+};
+
+return kanunlar[kelime] || "İlgili kanun bulunamadı.";
+
+}
